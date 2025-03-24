f6efe07e5a0b2b5705c61454f33ef856fa87501f
Dilip Vishwakarma <2106dilip@gmail.com>
2025-03-24 22:36:40 +0530

    add login functionality with token generation and cookie handling; update user routes to include login endpoint

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,10 +4,29 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import asyncHandler from "../utils/asyncHandler.js"
 
 
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findOne(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        user.save({validateBeforeSave:false})
+
+        return {accessToken, refreshToken}
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "Something went wrong while generating refresh and access token"
+        )
+    }
+}
+
+
 const registerUser = asyncHandler(async (req, res) => {
     // get user detail    
     const { fullName, email, username, password } = req.body
- 
+
     // validate - not empty
     let isEmpty = [fullName, email, username, password]
     isEmpty = isEmpty.some((feild) => {
@@ -17,7 +36,6 @@ const registerUser = asyncHandler(async (req, res) => {
     if (isEmpty) {
         throw new ApiError(400, "All feilds Required")
     }
-
 
     // check if user already exist: username, email
     const existedUser = await User.findOne(
@@ -30,7 +48,6 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "user with Email or Username already exists")
     }
 
-    
     // create user object - create entry in DB
     const user = await User.create(
         {
@@ -41,18 +58,15 @@ const registerUser = asyncHandler(async (req, res) => {
         }
     )
 
-
     // remove password and refresh token feild from response
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
 
-
     // check for user creation
     if (!createdUser) {
         throw new ApiError(500, "Somthing went wrong while registering the User")
     }
-
 
     // return response
     return res
@@ -62,7 +76,63 @@ const registerUser = asyncHandler(async (req, res) => {
         )
 })
 
+const loginUser = asyncHandler(async(req,res) => {
+    // get data
+    const {email, username, password} = req.body
+
+    // check for username or email
+    if(!(username||email)) {
+        throw new ApiError(400, "username or email is required")
+    }
+
+    // find the User
+    const user = await User.findOne({
+        $or:[{email},{username}],
+
+    })
+
+    if(!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+
+    // password check
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(402, "Invalid user credentials")
+    }
+
+    // generate access and refresh token
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+
+    // Send cookie response
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    const option = {
+        httpOnly : true,
+        secure: true
+    }
+
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken,option)
+        .cookie("accessToken", refreshToken,option)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUser, accessToken,refreshToken
+                },
+                "User Logged In Successfully"
+            )
+        )
+})
+
 
 export {
-    registerUser
+    registerUser,
+    loginUser
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, registerUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
 router.route("/register").post(upload.none(), registerUser)
+router.route("/login").post(loginUser)
 
 
 export default router
