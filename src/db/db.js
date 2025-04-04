7d97d4d8306956271a8a9adeabeda12c93c3b4ca
Dilip Vishwakarma <2106dilip@gmail.com>
2025-04-04 22:47:59 +0530

    remove debug logging for MONGODB_URI in connectDB function

--- src/db/db.js
+++ src/db/db.js
@@ -3,7 +3,6 @@ import { DB_NAME } from "../constants.js";
 
 
 const connectDB = async () => {
-    console.log("🔍 MONGODB_URI being used:", `${process.env.MONGODB_URI}/${DB_NAME}`);
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n Mongo DB connected  !! DB Host: ${connectionInstance.connection.host}`);
