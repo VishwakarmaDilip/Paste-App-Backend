ff844e86e09a91629b74e13dcfe4b054d73c3662
Dilip Vishwakarma <2106dilip@gmail.com>
2025-04-04 22:40:31 +0530

    add logging for MONGODB_URI in connectDB function for better debugging

--- src/db/db.js
+++ src/db/db.js
@@ -3,6 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 
 const connectDB = async () => {
+    console.log("🔍 MONGODB_URI being used:", `${process.env.MONGODB_URI}/${DB_NAME}`);
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n Mongo DB connected  !! DB Host: ${connectionInstance.connection.host}`);
